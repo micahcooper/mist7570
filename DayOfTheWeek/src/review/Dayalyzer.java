f63beebf59a047e10488722a7add6bd9bce2be1b
Micah Cooper <mc@micahcooper.com>
2016-01-24 18:44:58 -0500

    commit

--- DayOfTheWeek/src/review/Dayalyzer.java
+++ DayOfTheWeek/src/review/Dayalyzer.java
@@ -25,20 +25,29 @@ public class Dayalyzer {
 	}
 
 	public String analyze(){
-		int days, monthCounter = 0;
-		int year = ((this.year-1900)*365)+((this.year-1900)/4);
+		int days, monthCounter = 1;
+		days = ((this.year-1900)*365)+((this.year-1900)/4)+this.day;
+		System.out.println("Days "+days);
 		
 		if(year%4==0)
 			if(this.month==1 || this.month==2)
-				this.year =- 1;
+				days--;
 		
-		while(monthCounter != this.month-1 ){
-			switch (month){
-				case 1: days =+ 31;break;
-				case 2: days =+ 28;break;
-				case 3: days =+ 31;break
-				case 4: days =+ 30;break;
-				case 5: days =+
+		while(monthCounter != this.month ){
+			System.out.println(monthCounter);
+			switch (monthCounter++){
+				case 1: days += 31;break;
+				case 2: days += 28;break;
+				case 3: days += 31;break;
+				case 4: days += 30;break;
+				case 5: days += 31;break;
+				case 6: days += 30;break;
+				case 7: days += 31;break;
+				case 8: days += 31;break;
+				case 9: days += 30;break;
+				case 10: days += 31;break;
+				case 11: days += 30;break;
+				case 12: days += 0;break;
 			}
 		}
 		
