cc1a6575055a34600c9665e650fe55cd56da891d
Micah Cooper <mc@micahcooper.com>
2016-03-24 00:17:35 -0400

    commit

--- AFileReader/WebContent/index.jsp
+++ AFileReader/WebContent/index.jsp
@@ -10,10 +10,6 @@ c:import>
 3 fn tags of your choice
 -->
 
-<jsp:useBean id="reader" class="model.Reader" />
-<jsp:setProperty name="reader" property="filename" value="${param.test }" />
-<jsp:getProperty name="reader" property="available" />
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
@@ -34,7 +30,7 @@ ${ param.test }
    <c:out value="${name}"/><p>
 </c:forTokens>
 
-<form name="guessForm" action="index.jsp" method="post" onsubmit="return validateForm()">
+<form name="guessForm" action="doRead" method="post" onsubmit="return validateForm()">
 	<input type='text' name='test'>
 	<input type="submit" name="reset" value="RESET">
 </form>
--- AFileReader/src/model/Reader.java
+++ AFileReader/src/model/Reader.java
@@ -3,59 +3,45 @@
  */
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
+import java.net.URL;
 
 /**
- * @author mrcooper
+ * @author micah cooper
  *
  */
 public class Reader {
-	public int[] test;
-	private String filename;
-	boolean isAvailable;
+	private File file;
+	private URL url;
 	
-	/**
-	 * @return the isAvailable
-	 */
-	public boolean isAvailable() {
-		read("");
-		return isAvailable;
-	}
-
-	/**
-	 * @return the filename
-	 */
-	public String getFilename() {
-		return filename;
-	}
-
-	/**
-	 * @param filename the filename to set
-	 */
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-
 	/**
 	 * 
 	 */
-	public Reader() {
+	public Reader(URL url) {
 		System.out.println("reader created");
-		test = new int [5];
-		int [] test = {1,2,3,4,5};	
+		this.url = url;
 	}
-	
-	public void read(String filename){
-		if(filename.equals(""))
-			filename=this.filename;
+
+	/**
+	 * @param filename
+	 */
+	public void read(){
+		BufferedReader buffer;
 		
 		try{
-			FileReader read = new FileReader(filename);
+			file = new File( url.getPath() );
+			FileReader read = new FileReader(file);
+			char c;
+			//System.out.println( "filename: "+file.toString()+" - message: "+read.read() );
+			while ((c = (char)read.read()) != -1) {
+	            System.out.print( c );
+	         }
 			read.close();
 		}
 		catch(Exception e){
-			System.out.println(e.getMessage());
+			System.out.println( "ERROR: "+e.getMessage() );
 		}
 	}
-
 }
